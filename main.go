6c5a09368e273318067d26145a13afd33d292dde
fieldlee <ldpfieldlee@gmail.com>
2019-05-29 03:31:33 -0700

    fix call function

--- main.go
+++ main.go
@@ -22,9 +22,41 @@ func(lcc *LedgerChainCode)Invoke(stub shim.ChaincodeStubInterface)pb.Response{
 	switch  strings.ToLower(funcname)  {
 	case "version":
 		return shim.Success([]byte(common.VERSION))
-	case "account_register":
-		return services.AccountRegister()
+	case "account_check":
+		return services.AccountCheck(stub)
+	case "account_confirm":
+		return services.AccountConfirm(stub)
+	case "account_lock":
+		return services.AccountLock(stub)
+	case "account_unlock":
+		return services.AccountUNLock(stub)
+	case "token_create":
+		return services.TokenCreate(stub)
+	case "token_lock":
+		return services.TokenUpdateDisable(stub)
 
+	case "token_unlock":
+		return services.TokenUpdateEnable(stub)
+
+	case "token_history":
+		return services.TokenGetHistory(stub)
+
+	case "issue":
+		return services.LedgerIssue(stub)
+
+	case "transfer":
+		return services.LedgerTransfer(stub)
+
+	case "balance":
+		return services.LedgerGetBalance(stub)
+
+	case "history":
+		return services.LedgerGetHistory(stub)
+
+	case "scale":
+		return services.LedgerScale(stub)
+	default:
+		return shim.Error("function not define,please check function")
 	}
 
 	return shim.Success(nil)
